b04bb328ea6c65667d1de28edd291183790f080c
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2025-01-13 10:19:40 -0300

    v.1.7

--- angular-pokedex/src/app/pokemon-list/pokemon-list.component.ts
+++ angular-pokedex/src/app/pokemon-list/pokemon-list.component.ts
@@ -1,10 +1,11 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-pokemon-list',
   imports: [],
-  template: `<p>pokemon-list works!</p>`,
-  styleUrls: ['./pokemon-list.component.sass'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  templateUrl: './pokemon-list.component.html',
+  styleUrls: ['./pokemon-list.component.sass']
 })
-export class PokemonListComponent { }
+export class PokemonListComponent {
+
+}
